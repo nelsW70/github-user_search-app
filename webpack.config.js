c47a56e03437e518c78b1a634f9ca3f760e2c964
nelsW70 <nelsfoltz@gmail.com>
2018-11-17 16:00:02 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "development",
+
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{ loader: "babel-loader" }],
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
